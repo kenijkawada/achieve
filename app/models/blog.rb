c0656ea7dc4d55c36369e218d496ff90db80829e
kenjikawada <k.kawada.8.x@gmail.com>
2018-01-25 22:46:04 +0900

    blog.rbを編集

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,5 +2,6 @@ class Blog < ApplicationRecord
   validates :title, presence: true
   validates :content, length: {in: 1..140}
 
+  validates :user_id, presence: true
   belongs_to :user, optional: true, foreign_key: 'user_id'
 end
