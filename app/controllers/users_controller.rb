a83890280ed8816d7492c88ac902c91070f49855
kenjikawada <k.kawada.8.x@gmail.com>
2018-01-28 21:22:28 +0900

    編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.create(user_params)
     if @user.save
       redirect_to user_path(@user.id)
     else
