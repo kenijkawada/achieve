ffa8d0f7cb1c3cc6b88ea82156ee0641cec7f398
kenjikawada <k.kawada.8.x@gmail.com>
2018-02-01 00:00:14 +0900

    マイグレーションファイルを整理

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180131140229) do
+ActiveRecord::Schema.define(version: 20180131145908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,9 @@ ActiveRecord::Schema.define(version: 20180131140229) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "email"
     t.string "password_digest"
+    t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name_id"], name: "index_users_on_name_id"
   end
 
